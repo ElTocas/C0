3ca50c6d4e00bcfa13b42a2fc2c433bc157ce4e2
TommasoMartire <39244219+ElTocas@users.noreply.github.com>
2022-04-02 12:50:25 +0200

    asdsad

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from matplotlib.axis import YAxis
+from matplotlib.pyplot import ticklabel_format
 import streamlit as st
 import pandas as pd
 import datetime
 
 import plotly.express as px
 from os.path import exists
+
+
+st.set_page_config(layout="wide", page_title="Timer from Rachel call")
 
 filename = "this_data.csv"
 
@@ -25,22 +30,32 @@ if st.sidebar.button("aggiungi"):
 #try:
 if "nuovaPersona" in st.session_state:
     a = pd.DataFrame.from_dict(st.session_state["nuovaPersona"])
-    st.sidebar.dataframe(a)
-if st.sidebar.button("salva"):
-    if not exists(filename): 
-        a.to_csv(filename,index=False)
-    else:
-        a.to_csv(filename, mode='a', index=False, header=False)
-
+    temp = st.sidebar.container()
+    temp.caption("Recap")
+    temp.caption("nome: " + str(a["nome"][0]))
+    temp.caption("Cognome: " + str(a["cognome"][0]))
+    temp.caption("Phone: " + str(a["phone"][0]))
+    temp.caption("Data Iscrizione: " + str(a["data"][0]))
+    #st.sidebar.dataframe(a)
+    if st.sidebar.button("salva"):
+        if not exists(filename): 
+            a.to_csv(filename,index=False)
+        else:
+            a.to_csv(filename, mode='a', index=False, header=False)
+    
+    if st.sidebar.button("reset Person data"):        
+        del st.session_state["nuovaPersona"]
+        st.experimental_rerun()
 
 # Tempo trascorso
-Accettabile=5
+Accettabile=3
 Medio=8
 Inaccettabile=11
 
 
 
 st.session_state.data = pd.read_csv(filename)
+st.session_state.data.set_index(["id"])
 differenzaDate = []
 colori = []
 NomeECognome = []
@@ -54,33 +69,34 @@ for i,x in enumerate(st.session_state.data["data"]):
     NomeECognome.append(st.session_state.data.loc[i,'nome'] + " " + st.session_state.data.loc[i,'cognome'])
 
     if duration < Accettabile:
-        colori.append("ok")
+        colori.append("Accettabile")
     elif duration <Medio:
-        colori.append("care")
+        colori.append("Da risolvere")
     else :
-        colori.append("NOTOK")
+        colori.append("Urgente")
 
 
 st.session_state.data["tempoTrascorso"] = differenzaDate
 st.session_state.data["warnings"] = colori
 
-pf = px.bar(st.session_state.data, y=NomeECognome,x="tempoTrascorso",orientation='h',color="warnings",hover_data=["phone"])    
-st.dataframe(st.session_state.data)
+pf = px.bar(st.session_state.data, y=NomeECognome,x="tempoTrascorso",orientation='h',color = "warnings" ,hover_data=["phone"], color_discrete_map={"Accettabile": "green","Da risolvere": "blue","Urgente":"red"},opacity=0.6,text="tempoTrascorso")  
 st.plotly_chart(pf)
 
 
-c1,c2 = st.columns([4,1])
+c1,c2,cx,cx1,c3 = st.columns([3,2,2,1,2])
 if "index_to_delete" not in st.session_state:
     st.session_state.index_to_delete = []
 
 for i,x in enumerate(st.session_state.data["nome"]):
     text_to_show = x + " " + st.session_state.data.loc[i,"cognome"]
     if c1.button(text_to_show):
-        c2.text("RIMOSSO")
+        c2.text("da rimuovere")
         st.session_state.index_to_delete.append(i)
+    else:
+        c2.text("In lavorazione")
     
 
-if st.button("Delete selected"):
+if c3.button("Delete selected"):
     if st.session_state.index_to_delete is not []:
         newData = st.session_state.data.drop(st.session_state.index_to_delete)
         newData.to_csv(filename,index=False)
